49e4bd63f62bd3526713451acf18466152fe770e
Shianne T <53974564+ShianneJT@users.noreply.github.com>
2022-03-19 10:44:43 -0600

    adding link to github

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ export default function App() {
     <div className="App">
       <h1>Weather Search</h1>
       <Weather />
+      <span><a href="https://github.com/ShianneJT/weather-app-react" target="_blank" rel="noreferrer">Open-source code</a> by Shianne Taylor.
+      </span>
     </div>
   );
 }
